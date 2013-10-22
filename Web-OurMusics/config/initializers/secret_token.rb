af4e999aad2aa638bed83531b679da693ae605f3
TonGarcia <ton_unb@hotmail.com>
2013-10-22 02:11:24 -0200

    Commit com a atualização do projeto Web com a criação do projeto com o RSpec funcionando

---
+++ Web-OurMusics/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+WebOurmusics::Application.config.secret_key_base = '3177109ed3363728d1733e3fc88967bd1d4242c90f1b6e90fd597d0fa6629742fe3fd6e23df6937d4995e98ae3498cd3b7a088baba1f4dca05e0815cfaef67a0'
---
+++ Web-OurMusics/config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+WebOurmusics::Application.config.session_store :cookie_store, key: '_Web-OurMusics_session'
